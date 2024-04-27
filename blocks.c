92f3a4d88424303447b94ddfa901a57924ed1026
artisticmclaren <elijahjvandeventer@gmail.com>
2024-04-26 23:40:42 -0400

    block now spawns and drops and you can move the block with arrow keys

--- blocks.c
+++ blocks.c
@@ -23,9 +23,9 @@ void drawL(int cposx,int cposy) {
 
 void drawO(int cposx,int cposy) {
 	DrawRectangle(cposx,cposy,30,30,DARKGRAY);
-	DrawRectangle(cposx-30,cposy,30,30,DARKGRAY);
+	DrawRectangle(cposx+30,cposy,30,30,DARKGRAY);
 	DrawRectangle(cposx,cposy-30,30,30,DARKGRAY);
-	DrawRectangle(cposx-30,cposy-30,30,30,DARKGRAY);
+	DrawRectangle(cposx+30,cposy-30,30,30,DARKGRAY);
 }
 
 void drawT(int cposx,int cposy) {
--- main.c
+++ main.c
@@ -3,11 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+
+int main(void) { // this code is a mess but it works
 	int width = 1280;
 	int height = 720;
 	int whalf = width/2;
 	int hhalf = height/2;
+
+	bool lkeyup = true;
+	bool rkeyup = true;
+
+	int levels[] = {120,110,100,95,85,75,60,50,40,30,20,15};
+	int level=0;
+	int gridsize[] = {10,20};
+	int framespast = 0;
+	int cblockpos[] = {whalf-30,60};
 
 	InitWindow(width,height, "Tetris");
 	SetTargetFPS(120);
@@ -19,16 +29,39 @@ int main(void) {
 			int tw = MeasureText("Tetris",20);
 			DrawText("Tetris",whalf-tw/2,10,20,LIGHTGRAY);
 		}
-		DrawRectangle(whalf-150,60,300,600,LIGHTGRAY);
-		drawl(200,50);
-		drawJ(200,150);
-		drawL(200,250);
-		drawO(200,350);
-		drawT(200,450);
-		drawS(200,550);
-		drawZ(200,650);
+		DrawRectangle(whalf-150,60,30*gridsize[0],30*gridsize[1],LIGHTGRAY);
+		if (framespast==levels[level]) {
+			cblockpos[1]+=30;
+			framespast=0;
+		}
+
+		// reset rkeyup and lkeyup so you can press the button again
+		if (IsKeyUp(KEY_RIGHT)) rkeyup=true;
+		if (IsKeyUp(KEY_LEFT)) lkeyup=true;
+
+		// move current block
+		if (IsKeyDown(KEY_RIGHT) && rkeyup==true) {
+			cblockpos[0]+=30;
+			rkeyup=false;
+		}
+		
+		if (IsKeyDown(KEY_LEFT) && lkeyup==true) {
+			cblockpos[0]-=30;
+			lkeyup=false;
+		}
+
+		if (IsKeyDown(KEY_DOWN)) {
+			if (framespast % 2 == 0) cblockpos[1]+=30; // drops block one gridspace every 2 frames
+		}
+
+		if (IsKeyDown(KEY_SPACE)) {
+			// TODO: hard drop
+		}
+
+		drawO(cblockpos[0],cblockpos[1]);
 
 		EndDrawing();
+		framespast++;
 	}
 
 	CloseWindow();
